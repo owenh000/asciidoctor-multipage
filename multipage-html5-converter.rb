664c3a0c3fd1f4af93cbf0f4dd6fb0027b00aef1
Owen T. Heisler <owenh000@gmail.com>
2019-10-16 18:01:56 -0500

    Move extension header into README

--- multipage-html5-converter.rb
+++ multipage-html5-converter.rb
@@ -1,41 +1,4 @@
 # coding: utf-8
-#
-# Extends the Html5Converter to generate multiple pages from the document tree.
-#
-# Features:
-#
-# - Generates a root (top level) landing page with a list of child sections.
-# - Generates branch (intermediate level) landing pages as required, each with
-#   a list of child sections.
-# - Generates leaf (content level) pages with the actual content.
-# - Allows the chunking depth to be configured with the `multipage-level`
-#   document attribute (the default is 1—split into chapters).
-# - Supports variable chunking depth between sections in the document (by
-#   setting the `multipage-level` attribute on individual sections).
-# - Uses section IDs to name each page (eg. "introduction.html").
-# - Supports cross-references between pages.
-# - Generates a full Table of Contents for each page, but with relevant entries
-#   only (the TOC collapses as required for each page).
-# - Includes a description for each section on the branch/leaf landing pages
-#   (from the `desc` attribute, if set).
-# - Generates previous/up/home/next navigation links for each page.
-# - Allows the TOC entry for the current page to be styled with CSS.
-# - Supports standalone and embedded (--no-header-footer) HTML output.
-# - Retains correct section numbering throughout.
-#
-# Notes and limitations:
-#
-# - Tested with Asciidoctor 1.5.7.1; inline anchors in unordered list items
-#   require the fix for asciidoctor issue #2812.
-# - This extension is tightly coupled with Asciidoctor internals, and future
-#   changes in Asciidoctor may require updates here. Hopefully this extension
-#   exposes ways in which the Asciidoctor API can be improved.
-# - Footnotes are currently not supported.
-# - Please contribute fixes and enhancements!
-#
-# Usage:
-#
-#   asciidoctor -r ./multipage-html5-converter.rb -b multipage_html5 book.adoc
 
 require 'asciidoctor/converter/html5'
 
